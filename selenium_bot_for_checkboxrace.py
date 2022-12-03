cc83b5812c82d158a9cbd616616a39e09b14dcdf
selcukemreozer <selcuk.emre.ozer@outlook.com>
2022-12-03 15:59:23 +0300

    first commit

---
+++ selenium_bot_for_checkboxrace.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from time import sleep
+browser = webdriver.Chrome()
+url = "https://checkboxrace.com/"
+
+browser.get(url)
+sleep(2)
+for each in range(100):
+    sleep(0.02)
+    browser.find_element("xpath", "/html/body/div[1]/input["+str(each+1)+"]").click()
+
+#for i in range(10):
+#    try:
+#        browser.find_element_by_xpath()
+
